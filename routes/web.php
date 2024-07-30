dd09a0ea243b60adb1c841db6ee11051a698cc24
RoxanLagto <rpl1771@students.uc-bcf.edu.ph>
2024-07-30 04:14:17 -0400

    Update: Removed the welcome view and changed the route of blade to localhost

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/books', [BookController::class, 'index']);
+Route::get('/', [BookController::class, 'books']);
